c7c23d3a8d6189b8a6bd8415ddb32a7313151118
DemiOever <demisvnd@hotmail.com>
2025-07-17 14:47:08 +0200

    Made the main work with SequenceReader on a test sequence fasta file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,17 @@
 import picocli.CommandLine;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new CommandlineProcessor()).execute(args);
-        System.exit(exitCode);
+        SequenceReader reader = new SequenceReader();
+        try {
+            List<String> seqs = reader.readSequences(Paths.get("C:/Users/DemiS/Documents/School/Schooljaar_24_25/sequence.fasta"));
+            seqs.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println("Fout bij inlezen: " + e.getMessage());
+        }
     }
 
-    public static int multiply(int x, int y, int z) {
-        return x * y * z;
-    }
-
-    private int zValue;
-
-    public Main(int zValue){
-        this.zValue = zValue;
-    }
-
-    public void start(String[] args) {
-        int length = Integer.parseInt(args[0]);
-        int width = Integer.parseInt(args[1]);
-        int surface = multiply(length, width, this.zValue);
-        System.out.println("surface = " + surface);
-    }
 }
