2905e05c5a596bd6440f57f01cc17b011ab8087e
DemiOever <demisvnd@hotmail.com>
2025-08-18 21:24:43 +0200

    Changed ReturnFile

--- src/main/java/ReturnFile.java
+++ src/main/java/ReturnFile.java
@@ -1,9 +1,26 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.io.BufferedWriter;
 
 public class ReturnFile {
-    public void writeProfileToFile(String profile, Path outputPath) throws IOException {
-        Files.writeString(outputPath, profile);
+
+    /**
+     * Writes the IUPAC profile to both console and to the given file
+     *
+     * @param profile the IUPAC profile
+     * @param outputPath path to output file
+     * @throws IOException if writing to the file fails
+     */
+    public void writeProfile(String profile, Path outputPath) throws IOException {
+        System.out.println("Generated IUPAC Profile:");
+        System.out.println(profile);
+
+        try (BufferedWriter writer = Files.newBufferedWriter(outputPath)) {
+            writer.write(">IUPAC_Profile");
+            writer.newLine();
+            writer.write(profile);
+            writer.newLine();
+        }
     }
 }
