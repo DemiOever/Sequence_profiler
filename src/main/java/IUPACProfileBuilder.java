c77849df625765ee00f006e78edb6e91b816b6a0
DemiOever <demisvnd@hotmail.com>
2025-08-18 20:39:18 +0200

    Minor changes

--- src/main/java/IUPACProfileBuilder.java
+++ src/main/java/IUPACProfileBuilder.java
@@ -5,21 +5,30 @@ public class IUPACProfileBuilder {
     private static final Map<Set<Character>, Character> IUPAC_MAP = new HashMap<>();
 
     static {
+        // Single bases
         IUPAC_MAP.put(Set.of('A'), 'A');
         IUPAC_MAP.put(Set.of('C'), 'C');
         IUPAC_MAP.put(Set.of('G'), 'G');
         IUPAC_MAP.put(Set.of('T'), 'T');
+
+        // Double base combinations
         IUPAC_MAP.put(Set.of('A', 'G'), 'R');
         IUPAC_MAP.put(Set.of('C', 'T'), 'Y');
         IUPAC_MAP.put(Set.of('G', 'C'), 'S');
         IUPAC_MAP.put(Set.of('A', 'T'), 'W');
         IUPAC_MAP.put(Set.of('G', 'T'), 'K');
         IUPAC_MAP.put(Set.of('A', 'C'), 'M');
+
+        // Triple base combinations
         IUPAC_MAP.put(Set.of('A', 'C', 'G'), 'V');
         IUPAC_MAP.put(Set.of('A', 'C', 'T'), 'H');
         IUPAC_MAP.put(Set.of('A', 'G', 'T'), 'D');
         IUPAC_MAP.put(Set.of('C', 'G', 'T'), 'B');
+
+        // Quadruple base combinations
         IUPAC_MAP.put(Set.of('A', 'C', 'G', 'T'), 'N');
+
+        // Gaps
         IUPAC_MAP.put(Set.of('-'), '-'); // Gap
         IUPAC_MAP.put(Set.of('.'), '.'); // Gap
     }
