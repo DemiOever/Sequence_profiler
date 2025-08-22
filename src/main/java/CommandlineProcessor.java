5d77c0760596df32ace7029c671f09d56c08b45d
DemiOever <demisvnd@hotmail.com>
2025-08-22 09:59:49 +0200

    Changed CommandlineProcessor so it only loops over 1 input file

--- src/main/java/CommandlineProcessor.java
+++ src/main/java/CommandlineProcessor.java
@@ -3,17 +3,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
 import picocli.CommandLine;
-import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Option;
 
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @CommandLine.Command(name="SequenceProfiler", version="SequenceProfiler 0.1", mixinStandardHelpOptions = true)
 public class CommandlineProcessor implements Runnable {
     private static Logger logger = LogManager.getLogger(CommandlineProcessor.class.getName());
 
     @Option(names = {"-i", "--input-file"}, required = true)
-    private String[] inputFasta;
+    private String inputFasta;
 
     @Option(names = {"-op", "--output-profile"}, required = false)
     private String outputProfileFile;
@@ -33,12 +33,29 @@ public class CommandlineProcessor implements Runnable {
         }
         logger.debug("Processing input arguments ...");
 
-        logger.info("Input FASTA file(s): {}", Arrays.toString(inputFasta));
+        logger.info("Input FASTA file: {}", inputFasta);
         logger.info("Output profile file: {}", outputProfileFile);
-        logger.info("Output full result file: {}", outputFile);
-        logger.info("Verbosity: {}", Arrays.toString(verbose));
+        if (outputFile != null) {
+            logger.info("Full results file: {}", outputFile);
+        }
 
-        // Do something with the input
-        logger.debug("Program finished");
+        try {
+            Path inputPath = Path.of(inputFasta);
+            if (!Files.exists(inputPath)) {
+                logger.error("Input file does not exist: {}", inputPath);
+                return;
+            }
+
+            Path outputPath = Path.of(outputProfileFile);
+            logger.info("Processing file {} -> {}", inputPath, outputPath);
+
+            SequenceProcessor processor = new SequenceProcessor();
+            processor.process(inputPath, outputPath);
+
+            logger.info("Processing finished successfully");
+
+        } catch(Exception e){
+            logger.error("Failed to process {}: {}", inputFasta, e.getMessage(), e);
+        }
     }
 }
