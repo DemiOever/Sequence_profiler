bb17d72bfeb05f9896265aed41cdf5ec040ef3bc
DemiOever <demisvnd@hotmail.com>
2025-08-24 16:20:17 +0200

    Changed main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,8 @@
-import java.nio.file.*;
+import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
-                SequenceProcessor processor = new SequenceProcessor();
-
-        Path fastaFile = Paths.get("C:/Users/DemiS/Documents/School/Schooljaar_24_25/60_amniota_vertebrates_Mercator_Pecan.fa");
-        Path outputFile = Paths.get("C:/Users/DemiS/Documents/School/Schooljaar_24_25/profile.txt");
-
-        processor.process(fastaFile, outputFile);
+        int exitCode = new CommandLine(new CommandlineProcessor()).execute(args);
+        System.exit(exitCode);
     }
 }
