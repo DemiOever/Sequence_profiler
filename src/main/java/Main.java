049686af426ea87630f8558255dea94f84e8c060
DemiOever <demisvnd@hotmail.com>
2025-08-25 13:20:25 +0200

    Minimum change to main, so the loggers will show up in terminal.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
+        Configurator.setRootLevel(Level.DEBUG);
+
         int exitCode = new CommandLine(new CommandlineProcessor()).execute(args);
         System.exit(exitCode);
     }
